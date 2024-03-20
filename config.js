58d7675a68f544e81d68cfe0db487954e4756363
Madara-2 <155267436+Madara-2@users.noreply.github.com>
2024-03-20 14:50:12 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TOKEN: "MTIwNjIyOTY3Mjg0NTYzOTcxMA.Gc5TpX.M2XNy7ex7tYLZ2DnfYI1uwcaUltwn9dFcAgC0Q",
   ownerID: ["1004206704994566164", ""],
-  botInvite: "",
+  botInvite: "https://discord.com/oauth2/authorize?client_id=1206229672845639710&permissions=277028550656&scope=bot",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   status: 'ARL8 | help',
